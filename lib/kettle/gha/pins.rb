c633526495c7db0a8721a94a71c3def0f3cc71bb
Peter H. Boling <peter.boling@gmail.com>
2026-07-22 13:58:23 -0600

    🎉 TADA

---
+++ lib/kettle/gha/pins.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "pins/version"
+
+module Kettle
+  module Gha
+    module Pins
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/kettle/gha/pins/version.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Kettle
+  module Gha
+    module Pins
+      VERSION = "0.1.0"
+    end
+  end
+end
