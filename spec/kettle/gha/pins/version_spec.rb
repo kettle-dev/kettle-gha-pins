e1ffbef816d02fc67a095ee7692e894938f361da
Peter H. Boling <peter.boling@gmail.com>
2026-07-30 21:56:08 -0600

    🎨 Template bootstrap by kettle-jem v7.1.0

---
+++ spec/kettle/gha/pins/version_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require "anonymous_loader"
+RSpec.describe Kettle::Gha::Pins::Version do
+  it_behaves_like "a Version module", described_class
+
+  it "executes the version file for coverage without redefining constants" do
+    paths = [
+      File.expand_path("../../../../lib/kettle/gha/pins/version.rb", __dir__),
+      File.expand_path("../../../../lib/kettle/gha/pins/version_gem.rb", __dir__)
+    ].select { |path| File.file?(path) }
+    anonymous_namespace = AnonymousLoader.load(files: paths)
+
+    expect(anonymous_namespace::Kettle::Gha::Pins::Version::VERSION).to eq(described_class::VERSION)
+  end
+end
