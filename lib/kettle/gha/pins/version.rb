3e2b1d0983d03895739e563e856a370dda591b2c
Peter H. Boling <peter.boling@gmail.com>
2026-08-05 19:37:57 -0600

    🔖 Bump gem version

--- lib/kettle/gha/pins/version.rb
+++ lib/kettle/gha/pins/version.rb
@@ -6,7 +6,7 @@ module Kettle
       # Version namespace for this gem.
       module Version
         # Current gem version.
-        VERSION = "0.3.8"
+        VERSION = "0.3.9"
       end
       # Current gem version exposed at the traditional constant location.
       VERSION = Version::VERSION # Traditional Constant Location
